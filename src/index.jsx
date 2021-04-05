b84c350376502051dfda512d9a559774394dc35e
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-04-05 15:22:34 -0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./pages/home";
+
+import "./assets/css/index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
