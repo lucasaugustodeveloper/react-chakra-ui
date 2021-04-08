0bfc6edbe199427f65fef9d252572807f80c7210
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-04-08 10:06:09 -0300

    add container and grid

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import { Container, Grid, GridItem } from '@chakra-ui/react';
+
 function App() {
-  return <div>home</div>;
+  return (
+    <Container maxW="container.xl">
+      <Grid h="100px" templateRows="repeat(2, 1fr)" gap={4}>
+        <GridItem bg="tomato" />
+        <GridItem bg="purple" />
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
